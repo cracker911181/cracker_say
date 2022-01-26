1e14a99a6eebec64b0c9a8f216125feb4bf56178
Cracker <85168111+cracker911181@users.noreply.github.com>
2022-01-26 10:41:55 +0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='cracker_say',
 version='1.5',
-description='This PIP only for Encoding Cracker File',
+description='This PIP only made for designing Tool and Termux Bennar',
 author='cracker911181',
 url='https://github.com/cracker911181/cracker_enc',
 packages=['cracker_say'],
